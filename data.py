afd42cac54d72db8064f3699a24a11c7afc40b83
rk-izak <102994642+rk-izak@users.noreply.github.com>
2023-02-18 20:52:01 +0100

    data script update

--- data.py
+++ data.py
@@ -1,8 +1,25 @@
 import pandas as pd
-# "bookId","title","series","author","rating","description","language","isbn","genres","characters",
-# "bookFormat","edition","pages","publisher","publishDate","firstPublishDate",
-# "awards","numRatings","ratingsByStars","likedPercent","setting","coverImg","bbeScore","bbeVotes","price"
 
-df = pd.read_csv("books_data.csv", usecols = ['title','series', 'author', 'rating', 'language', 
-                                              'genres', 'characters','publisher', 'pages', 'likedPercent','price'])
-print(df)
+"""
+All possible dictionary entries are:
+
+bookId, title, series, author, rating, description, language, isbn, genres, characters, 
+bookFormat, edition, pages, publisher, publishDate, firstPublishDate, 
+awards, numRatings, ratingsByStars, likedPercent, setting, coverImg, bbeScore, bbeVotes, price
+"""
+
+"""
+All currently used entries are:
+
+title, series,  author,  rating,  language,  
+genres,  characters, publisher,  pages,  likedPercent, price
+"""
+
+def get_data(data_file):
+    df = pd.read_csv(data_file, usecols = ['title','series', 'author', 'rating', 'language', 
+                            'genres', 'characters','publisher', 'pages', 'likedPercent','price'])
+    # print(df[['characters']])
+    return df
+    
+
+get_data("books_data.csv")
